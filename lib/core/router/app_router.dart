add05cc41822ff1f304d8091ce37a34e9b327a2a
BALOLBILO <octavianosz@gmail.com>
2025-10-08 23:16:06 -0300

    FIRESTORE

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_application_tp/entities/usuarios.dart';
 import 'package:flutter_application_tp/presentation/screens/Agregar_screen.dart';
 import 'package:flutter_application_tp/presentation/screens/Cambiar_screen.dart';
 import 'package:flutter_application_tp/presentation/screens/Descripcion_screen.dart';
